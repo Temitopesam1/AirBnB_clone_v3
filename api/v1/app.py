9fd096f77432a14f200965d0dd70358c647b2097
Temitopesam1 <temitopesam5@gmail.com>
2023-01-22 12:26:49 +0000

    second push

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 
 app = Flask(__name__)
@@ -13,5 +14,5 @@ def session_teardown(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host=(os.getenv('HBNB_API_HOST'), "0.0.0.0"),
-            port=(os.getenv('HBNB_API_PORT'), 5000), threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', "0.0.0.0"),
+            port=os.getenv('HBNB_API_PORT', '5000'), threaded=True)
